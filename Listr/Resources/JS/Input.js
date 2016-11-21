455745b22cd5e2a07557eae552575d4b3365856c
Matt Dwyer <mluckydwyer@gmail.com>
2016-11-21 12:30:56 -0600

    Added enter to push button

--- Listr/Resources/JS/Input.js
+++ Listr/Resources/JS/Input.js
@@ -2,6 +2,16 @@
  * Created by mluck on 11/18/2016.
  */
 
+function setup() {
+    document.getElementById('center-main-input').onkeypress=function(e){
+        if(e.keyCode==13){
+            document.getElementById('make-list-button').click();
+        }
+    }
+}
+
+
+
 function parse(input) {
     console.log(input)
     showSnackbar(input)
